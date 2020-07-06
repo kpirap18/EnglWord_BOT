6cebcb393d0c91be1cf6f148df7e0dc815ab78d8
kpirap18 <ir1nak0z@yandex.ru>
2020-07-06 22:53:08 +0300

    Add bot

--- bot.py
+++ bot.py
@@ -1,11 +1,24 @@
 import telebot
+import config
 
-token = '1225876441:AAHzRWvPtXz84nGdiTL90V12912KWogfK2M'
+bot = telebot.TeleBot(config.TOKEN)
 
-bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=["help"])
+def help_messages(message):
+
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    button1 = telebot.types.InlineKeyboardButton(
+        u'\U0001f525' "Для связи с разработчиком",
+        url='telegram.me/k_ira_18')
+    keyboard.add(button1)
+    bot.send_message(message.chat.id,
+                     "1 - Для начала работы нажмите ▶️ /start\n\n"
+                     "2 - Если есть вопросы, пиши разработчикам",
+                     reply_markup= keyboard)
+
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
-
 if __name__ == '__main__':
     bot.polling(none_stop=True)
